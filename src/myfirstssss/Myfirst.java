6c63836297e769449adce3f573a71dc80a4e2efa
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2021-02-25 06:49:51 -0800

    # omor java sweetooo obi is aboy

---
+++ src/myfirstssss/Myfirst.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// the project is icehub
+
+package myfirstssss;
+
+/**
+ *
+ * @author HP
+ */
+//this is  like the  class that is holding
+//this is  software development
+public class Myfirst {
+
+    /**
+     * @param args the command line arguments
+     */
+//     this is like the topic
+    public static void main(String[] args) {
+        System.out.println("charlse babage"); 
+            System.out.println("charlse babage"); 
+    System.out.println("charlse babage"); 
+    System.out.print("charlse babage");   System.out.print("charlse babage");
+  System.out.print("charlse babage");
+  System.out.print("charlse babage");// TODO code application logic here
+    }
+    
+}
